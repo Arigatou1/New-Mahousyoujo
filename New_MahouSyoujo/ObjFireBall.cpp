cfba0c693ccc86e7163c7aeb1a25edf780dacbe3
あほ りみに〜 <riminie996@yahoo.co.jp>
2020-11-30 09:35:22 +0900

    a

--- New_MahouSyoujo/ObjFireBall.cpp
+++ New_MahouSyoujo/ObjFireBall.cpp
@@ -93,7 +93,7 @@ void CObjFireBall::Action()
 void CObjFireBall::Draw()
 {
 	//描画カラー情報
-	float c[4] = { 1.0f,0.0f,0.0f,hitTime / 100.0f};
+	float c[4] = { 1.0f,1.0f,1.0f,hitTime / 100.0f};
 
 	RECT_F src;//描画元切り取り位置
 	RECT_F dst;//描画先表示位置
